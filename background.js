311e9bd6d996191a3a0e2cf35b8671bf390c3ccb
Ngo Hoang <nzo8oanz34@gmail.com>
2025-04-14 12:03:19 +0700

    feat: toggle annotation visibility by clicking the extension icon

--- background.js
+++ background.js
@@ -1,6 +1,23 @@
-chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.insertCSS({
-        target: { tabId: tab.id },
-        css: `tr.js-inline-annotations { display: none !important; }`
-    });
+// background.js
+
+let isEnabled = false;
+
+chrome.action.onClicked.addListener(async (tab) => {
+    if (!tab.id) return;
+
+    isEnabled = !isEnabled;
+
+    if (isEnabled) {
+        await chrome.scripting.insertCSS({
+            target: { tabId: tab.id },
+            css: `tr.js-inline-annotations { display: none !important; }`
+        });
+        chrome.action.setIcon({ path: "icons/icon-on.png", tabId: tab.id });
+    } else {
+        await chrome.scripting.removeCSS({
+            target: { tabId: tab.id },
+            css: `tr.js-inline-annotations { display: none !important; }`
+        });
+        chrome.action.setIcon({ path: "icons/icon-off.png", tabId: tab.id });
+    }
 });
